eeb6812ffb521db995962ee91ce4031f8c1d7343
Gregory <Colombe.Gregory@gmail.com>
2020-06-03 11:20:21 +0200

    Regroupement de notre travail
    Ajout de ce que j'avais fait (cinematic, systeme dialogue, reglage controller...)

---
+++ Assets/Scripts/Cinematic/CameraSwitch.cs
@@ -0,0 +1,58 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class CameraSwitch : MonoBehaviour
+// {
+//     public GameObject cameraOne;
+//     public GameObject cameraTwo;
+//     private float CinematicTimer = 4.5f;
+//     private float ChangeCameraTimer;
+//     [SerializeField] private Canvas textExplication;
+//     private float timeToDisplayExplication;
+//     private bool active = true;
+
+//     void Start()
+//     {
+//         textExplication.enabled = false;
+//     }
+
+//     void Update()
+//     {
+
+//         if (active == true)
+//         {
+//             CinematicTimer -= Time.deltaTime;
+//             ChangeCameraTimer -= Time.deltaTime;
+//             timeToDisplayExplication -= Time.deltaTime;
+//             Debug.Log("timeToDisplayExplication : " + timeToDisplayExplication);
+
+//             if (CinematicTimer >= 0)
+//             {
+//                 Debug.Log("ok BBb ");
+//                 cameraOne.transform.Translate(Vector3.forward * Time.deltaTime * 9);
+
+//                 ChangeCameraTimer = 0.3f;
+//                 timeToDisplayExplication = 8f;
+//             }
+
+//             if (ChangeCameraTimer <= 0)
+//             {
+//                 Debug.Log("ok changer de cam");
+//                 textExplication.enabled = true;
+//                 // cameraOne.SetActive(false);
+//                 // cameraTwo.SetActive(true);
+//             }
+
+//             if (timeToDisplayExplication <= 0)
+//             {
+//                 Debug.Log("zouzou");
+//                 textExplication.enabled = false;
+//                 cameraOne.SetActive(false);
+//                 cameraTwo.SetActive(true);
+//                 active = false;
+//             }
+//         }
+
+//     }
+// }
--- Assets/Scripts/UI/ChoiceScript.cs
+++ Assets/Scripts/UI/ChoiceScript.cs
@@ -4,34 +4,95 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ChoiceScript : MonoBehaviour
 {
+    [SerializeField] private Canvas customImage;
     public GameObject TextBox;
     public GameObject Choice01;
     public GameObject Choice02;
     public GameObject TextChoice01;
     public GameObject TextChoice02;
-    public int ChoiceMade;
 
-    public void ChoiceOption1()
+    public GameObject Choice03;
+    public GameObject Choice04;
+    public GameObject TextChoice03;
+    public GameObject TextChoice04;
+    public int ChoiceMade;
+    private bool noButton;
+    private string initialText;
+
+    void Start()
     {
-        TextBox.GetComponent<Text>().text = "Le prix de la bouteille est de 2,5$, veut tu l'acheter ?";
-        // TextChoice01.GetComponent<Text>().text = "wook";
-        // TextChoice02.GetComponent<Text>().text = "nanana";
+        initialText = TextBox.GetComponent<Text>().text;
+        Choice03.SetActive(false);
+        Choice04.SetActive(false);
+    }
+    public void ChoiceOption1_Dialogue1()
+    {
+
+        TextBox.GetComponent<Text>().text = "Le prix du pack est de 7,5$";
+        Choice01.SetActive(false);
+        Choice02.SetActive(false);
+        Choice03.SetActive(true);
+        Choice04.SetActive(true);
+        // TextChoice01.GetComponent<Text>().text = "Acheter";
+        // TextChoice02.GetComponent<Text>().text = "Voler";
 
         ChoiceMade = 1;
     }
-    public void ChoiceOption2()
+    public void ChoiceOption2_Dialogue1()
     {
         TextBox.GetComponent<Text>().text = "Dommage, bonne journée et à bientôt !";
-        // TextChoice01.GetComponent<Text>().text = "wook2";
-        // TextChoice02.GetComponent<Text>().text = "nanana2";
+
+        Choice01.SetActive(false);
+        Choice02.SetActive(false);
+        Choice03.SetActive(false);
+        Choice04.SetActive(false);
+        // noButton = true;
         ChoiceMade = 2;
+
+    }
+
+    //Dialogue 2 
+    public void ChoiceOption1_Dialogue2()
+    {
+        TextBox.GetComponent<Text>().text = "Merci pour votre achat, au revoir.";
+        Choice01.SetActive(false);
+        Choice02.SetActive(false);
+        Choice03.SetActive(false);
+        Choice04.SetActive(false);
+    }
+    public void ChoiceOption2_Dialogue2()
+    {
+        TextBox.GetComponent<Text>().text = "Ne reviens jamais ici voleur, j'espère que tu va perdre beaucoup de points !";
+        Choice01.SetActive(false);
+        Choice02.SetActive(false);
+        Choice03.SetActive(false);
+        Choice04.SetActive(false);
     }
     void Update()
     {
         if (ChoiceMade >= 1)
         {
-            Choice01.SetActive(false);
-            Choice02.SetActive(false);
+            // Choice01.SetActive(false);
+            // Choice02.SetActive(false);
         }
+
+        if (ChoiceMade == 2)
+        {
+            if (customImage.enabled == false)
+            {
+                TextBox.GetComponent<Text>().text = initialText;
+                Choice01.SetActive(true);
+                Choice02.SetActive(true);
+                // Choice03.SetActive(true);
+                // Choice04.SetActive(true);
+            }
+        }
+
+        // if (noButton == true)
+        // {
+        //     Choice01.SetActive(true);
+        //     Choice02.SetActive(true);
+        // }
+
     }
 }
